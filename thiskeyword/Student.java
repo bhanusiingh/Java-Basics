e7f57ceea11deed7dc0c0611c6f173124a20b982
bhanusiingh <bhanusingh.vivo@gmail.com>
2026-02-23 09:37:49 +0530

    THIS keyword

--- thiskeyword/Student.java
+++ thiskeyword/Student.java
@@ -1,3 +1,8 @@
+//this keyword is used to refer to the current object.
+//it means this refers to the objesct that is currently executing the code.
+//we can use this keyword to refer to the current object, to call the current object's method, to call the current object's constructor, to pass the current.
+// It also use to return the current object.
+//it is also used ti pass curent object as a parameter to another method or constructor.
 package thiskeyword;
 public class Student {
     String name;
